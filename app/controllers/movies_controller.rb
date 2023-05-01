2112f4efc05dcf75a33b5bbc9756b14f6b41ceca
AkihiroTanaka3 <qqqwwetyu@gmail.com>
2023-05-01 07:00:45 +0000

    commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,11 @@
 class MoviesController < ApplicationController
+  require 'themoviedb-api'
+  Tmdb::Api.key("00ab8363524fc4eb21c71b11eae329c1")
+  Tmdb::Api.language("ja") # こちらで映画情報の表示の際の言語設定を日本語にできます
+
   def search
   end
-
+  
   def show
   end
 end
---
+++ app/views/index.rb
@@ -0,0 +1,5 @@
+require 'ostruct'
+# 構造体を作って動的にpropertyを追加
+son = OpenStruct.new
+son.name = "Thomas"
+son.age = 3
